a7b9583b4ef2b883085df1d82036352a6d074e2b
Ritvik Divanji <ritvikdivanji@gmail.com>
2020-08-27 20:36:14 -0500

    Switching to light/dark/auto theming now works, but requires app restart because MyApp is not stateful.

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,28 @@
 import 'package:flutter/material.dart';
 import 'database_helper.dart';
 import 'homescreen.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   DatabaseService initialDB = DatabaseService();
   await initialDB.initDB();
-  runApp(MyApp());
+
+  SharedPreferences _prefs = await SharedPreferences.getInstance();
+  String value = _prefs.getString("darkMode") ?? "sys";
+  if(value == "sys")
+    runApp(MyApp(themeMode: ThemeMode.system));
+  else if(value == "on")
+    runApp(MyApp(themeMode: ThemeMode.dark));
+  else
+    runApp(MyApp(themeMode: ThemeMode.light));
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+  final ThemeMode themeMode;
+
+  const MyApp({@required this.themeMode});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -26,6 +38,7 @@ class MyApp extends StatelessWidget {
           foregroundColor: Colors.black,
         ),
       ),
+      themeMode: themeMode,
       home: HomeScreen(),
     );
   }
